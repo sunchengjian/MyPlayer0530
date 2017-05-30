39290223e9b7bf7bce33b7c8873c71718eedf61e
sunchengjian <3230706893@qq.com>
2017-05-30 11:06:11 +0800

    得到本地所有视频，并列表显示出来

--- app/src/main/java/com/sunchengjian/myplayer0530/pager/LocalVideo.java
+++ app/src/main/java/com/sunchengjian/myplayer0530/pager/LocalVideo.java
@@ -1,31 +1,102 @@
 package com.sunchengjian.myplayer0530.pager;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.sunchengjian.myplayer0530.R;
+import com.sunchengjian.myplayer0530.adapter.LocalVideoAdapter;
+import com.sunchengjian.myplayer0530.domain.MediaItem;
 import com.sunchengjian.myplayer0530.fragment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by 0..0 on 2017/5/30.
  */
 
 public class LocalVideo extends BaseFragment {
-    private TextView textView;
+    private ListView lv;
+    private TextView tv_nodata;
+    private ArrayList<MediaItem> mediaItems;
+    private LocalVideoAdapter adapter;
+
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_local_video, null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //  MediaItem mediaItem = mediaItems.get(position);
+                MediaItem item = (MediaItem) adapter.getItem(position);
+
+            }
+        });
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("111");
+        //加载本地所有视频
+        getData();
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (mediaItems != null && mediaItems.size() > 0) {
+                tv_nodata.setVisibility(View.GONE);
+                adapter = new LocalVideoAdapter(context, mediaItems);
+                lv.setAdapter(adapter);
+
+            } else {
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+    public void getData() {
+        new Thread() {
+            public void run() {
+                mediaItems = new ArrayList<>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,
+                        MediaStore.Video.Media.DURATION,
+                        MediaStore.Video.Media.SIZE,
+                        MediaStore.Video.Media.DATA,
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                        Log.e("TAG", "name==" + name + ",duration==" + duration + ",data===" + data);
+
+                        mediaItems.add(new MediaItem(name, duration, size, data));
+
+                        handler.sendEmptyMessage(0);
+
+                    }
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
 
