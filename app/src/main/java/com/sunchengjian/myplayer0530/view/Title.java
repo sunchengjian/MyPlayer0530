297f72d1becdbfd557054715ee8127f9f80048a2
sunchengjian <3230706893@qq.com>
2017-05-30 10:52:32 +0800

    总框架布局完成，下一步本地视频

--- app/src/main/java/com/sunchengjian/myplayer0530/view/Title.java
+++ app/src/main/java/com/sunchengjian/myplayer0530/view/Title.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sunchengjian.myplayer0530.R;
 
 /**
  * Created by 0..0 on 2017/5/30.
@@ -12,6 +18,9 @@ import android.widget.LinearLayout;
 
 public class Title extends LinearLayout implements View.OnClickListener {
     private Context context;
+    private TextView tv_sousuo;
+    private RelativeLayout rl_game;
+    private ImageView iv_record;
 
     public Title(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -19,7 +28,30 @@ public class Title extends LinearLayout implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onFinishInflate() {
+        super.onFinishInflate();
 
+        tv_sousuo = (TextView) getChildAt(1);
+        rl_game = (RelativeLayout) getChildAt(2);
+        iv_record = (ImageView) getChildAt(3);
+
+        tv_sousuo.setOnClickListener(this);
+        rl_game.setOnClickListener(this);
+        iv_record.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_sousuo:
+                Toast.makeText(context, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_game:
+                Toast.makeText(context, "游戏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.iv_record:
+                Toast.makeText(context, "记录", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
